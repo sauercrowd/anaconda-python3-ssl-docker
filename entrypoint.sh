3da3164255baa5709f71eef5c7552d967dd3d4a2
sauercrowd <jonadev95@posteo.org>
2017-09-18 15:56:02 +0200

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Generate ssl certs if not exist
-if [[ ! -f /home/conda/ssl/jupyter.key ]]
+if [ ! -f /home/conda/ssl/jupyter.key ]
 then
 	openssl req -x509 -nodes -days 365 -subj "/C=DE/ST=Sauercrowd/L=Sauercrowd/O=Sauercrowd/OU=IT Department/CN=example.com" -newkey rsa:2048 -keyout /home/conda/ssl/jupyter.key -out /home/conda/ssl/jupyter.crt
 fi
 
 # Generate a token file if it doesn't exist
-if [[ ! -f /home/jotten/.jupyter/jupyter_notebook_config.py ]]
+if [ ! -f /home/jotten/.jupyter/jupyter_notebook_config.py ]
 then
 	TOKEN=`pwgen 20 1`
 	echo "c.NotebookApp.token = \'$TOKEN\'" > /home/conda/.jupyter/jupyter_notebook_config.py
